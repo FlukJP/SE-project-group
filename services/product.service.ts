cc831a1faa4e8ad9ac83fa9d8c5818875ffbef9c
FlukJP <FlukJP@example.com>
2026-02-22 15:01:06 +0700

    save

---
+++ services/product.service.ts
@@ -0,0 +1,19 @@
+import { Product } from "@/types/Product";
+import { ProductModel } from "@/models/productModel";
+import { AppError } from "@/errors/AppError";
+import { Filter } from "firebase-admin/firestore";
+
+export const ProductService = {
+    // 1.Product list
+    // 2.Product by seller
+    // 3.Product detail
+    // 4.search product
+    // 5.Create product
+    // 6.Change product status
+    // 7.Update product
+    // 8.Delete product
+    // 9.Report product
+    // 10.Upload product image
+    // 11.Remove product image
+    // 12.GET categories
+}
