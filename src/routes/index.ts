c22260dddce9a9d36802ff2f8d7990d80a60d926
FlukJP <FlukJP@example.com>
2026-03-07 12:02:22 +0700

    Save index.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import productRoutes from './product.routes';
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
 import chatRoutes from './chat.routes';
+import adminRoutes from './admin.routes';
+import reportRoutes from './report.routes';
 
 const router = Router();
 
@@ -10,5 +12,7 @@ router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/chats', chatRoutes);
+router.use('/admin', adminRoutes);
+router.use('/reports', reportRoutes);
 
 export default router;
