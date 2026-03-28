ff85ea17af79527a5cdacba88ee541aa7ff261bf
FlukJP <FlukJP@example.com>
2026-03-28 17:25:45 +0700

    Delete .replace(/^http/, 'ws').

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000').replace(/^http/, 'ws');
+const SOCKET_URL = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000');
 
 let _socket: Socket | null = null;
 
